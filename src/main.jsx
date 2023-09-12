702b85036ddd7b2dc96630c9258a1ac003ddefba
Marlyemarcio1 <luan.rdias@gmail.com>
2023-09-11 22:32:23 -0300

    Criação rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from './routes/Home/index.jsx'
+import Solicitacao from './routes/Solicitacao/index.jsx'
+import Membros from './routes/Membros/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    Element: <App/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path: "/soliticicao",
+        element: <Solicitacao/>
+      },
+      {
+        path: "/membros",
+        element: <Membros/>
+
+      }
+
+
+
+    ]
+  }])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
